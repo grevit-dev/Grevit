af8283d3aacff85807ad781f6388aeb457672a3f
moethu <maximilian.o.e.thumfart@gmail.com>
2017-07-11 18:39:13 +0200

    Added Face Query Tool

--- Grevit.Revit/Revit.cs
+++ Grevit.Revit/Revit.cs
@@ -83,6 +83,15 @@ namespace Grevit.Revit
 
             parameterButton.SetContextualHelp(new ContextualHelp(ContextualHelpType.Url, "http://grevit.net/"));
 
+            PushButton getFaceRefButton = grevitPanel.AddItem(new PushButtonData("GetFaceReference", "Face Reference", path, "Grevit.Revit.GrevitFaceReference")) as PushButton;
+            getFaceRefButton.LargeImage = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                Properties.Resources.radio_button.GetHbitmap(),
+                IntPtr.Zero,
+                System.Windows.Int32Rect.Empty,
+                BitmapSizeOptions.FromWidthAndHeight(32, 32));
+
+            getFaceRefButton.SetContextualHelp(new ContextualHelp(ContextualHelpType.Url, "http://grevit.net/"));
+
             return Result.Succeeded;
         }
 
@@ -110,6 +119,52 @@ namespace Grevit.Revit
 
     }
 
+
+    [Transaction(TransactionMode.Manual)]
+    public class GrevitFaceReference : IExternalCommand
+    {
+
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            // Get Environment Variables
+            UIApplication uiApp = commandData.Application;
+            Reference reference = uiApp.ActiveUIDocument.Selection.PickObject(ObjectType.Face, "Select Face");
+            if (reference != null)
+            {
+                string stable = reference.ConvertToStableRepresentation(uiApp.ActiveUIDocument.Document);
+                Prompt.ShowDialog("Stable Reference:", stable);
+            }
+
+            return Result.Succeeded;
+        }
+
+    }
+
+    public static class Prompt
+    {
+        public static string ShowDialog(string text, string caption)
+        {
+            System.Windows.Forms.Form prompt = new System.Windows.Forms.Form()
+            {
+                Width = 500,
+                Height = 150,
+                FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog,
+               // Text = caption,
+                StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen,
+                Text = ""
+            };
+            System.Windows.Forms.Label textLabel = new System.Windows.Forms.Label() { Left = 50, Top = 20, Text = text };
+            System.Windows.Forms.TextBox textBox = new System.Windows.Forms.TextBox() { Left = 50, Top = 50, Width = 400 , ReadOnly = true, Text = caption};
+            System.Windows.Forms.Button confirmation = new System.Windows.Forms.Button() { Text = "Ok", Left = 350, Width = 100, Top = 70, DialogResult = System.Windows.Forms.DialogResult.OK };
+            confirmation.Click += (sender, e) => { prompt.Close(); };
+            prompt.Controls.Add(textBox);
+            prompt.Controls.Add(confirmation);
+            prompt.Controls.Add(textLabel);
+            prompt.AcceptButton = confirmation;
+
+            return prompt.ShowDialog() == System.Windows.Forms.DialogResult.OK ? textBox.Text : "";
+        }
+    }
 
     /// <summary>
     /// The actual Revit Grevit Command
